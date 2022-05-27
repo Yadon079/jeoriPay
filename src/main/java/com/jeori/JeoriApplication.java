5d8754d640ebff02be3b5372707b4a3e83859c17
gracenam <thankowl777@kakao.com>
2022-05-27 12:33:57 +0900

    feature : 프로젝트 생성
    <description>
    * 저리 페이
    - 프로젝트 생성
    
    <issue-number>

---
+++ src/main/java/com/jeori/JeoriApplication.java
@@ -0,0 +1,13 @@
+package com.jeori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeoriApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JeoriApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/jeori/JeoriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeori;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeoriApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
